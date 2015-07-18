9e64ce931a60bece7ea7e1daf6561097a710230e
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-07-18 17:16:04 -0500

    require webpack, add buildWebpack func in buildScripts func.

--- gulp/build/scripts.js
+++ gulp/build/scripts.js
@@ -1,10 +1,26 @@
 'use strict';
 var gulp = require('gulp');
 var debug = require('gulp-debug');
+var webpack = require('webpack');
+var config = require('../../webpack.config.js');
 
-function buildScripts() {
-  console.log('building scripts');
-  return gulp.src('');
+function buildScripts(callback) {
+  //console.log('building scripts');
+
+function buildWebpack(err, stats){
+  if(err) {
+     console.log('Error', err);
+   }
+   else {
+     console.log(stats.toString());
+   }
+   callback();
+}
+
+webpack(config)
+.run(buildWebpack);
+
+  //return gulp.src('');
 }
 
 gulp.task('build-scripts', buildScripts);
