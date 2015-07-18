f32a1cee595ad1f698aa070589b23025eb86c063
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-07-18 15:29:23 -0500

    add read me, contributing, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var path = require('path');
+
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'js'),
+    publicPath: 'assets/', // relative path for github pages
+    filename: 'index.js', // no hash in main.js because index.html is a static page
+    chunkFilename: '[hash]/js/[id].js',
+    hotUpdateMainFilename: '[hash]/update.json',
+    hotUpdateChunkFilename: '[hash]/js/[id].update.js'
+  }
+};
