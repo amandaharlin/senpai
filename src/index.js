cb1161dd4fc4960ab3a856024d35b976898c5374
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-07-25 13:14:31 -0500

    add child state katy.blog

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ var app = angular.module('app', ['ui.router']);
 function katyStateProvider($stateProvider){
   var katy = {
     url: 'katy',
-    template: '<span>Katy rules the roost!</span>'
+    template: '<span>Katy rules the roost! <div ui-view></div></span>'
   }
-  
+
   $stateProvider.state('katy', katy)
+}
+
+function katyBlogStateProvider($stateProvider) {
+  var katyBlogState = {
+    url: '/blog',
+    template: '<span>dis ma blawg</span>'
+  }
+  $stateProvider.state('katy.blog', katyBlogState);
 }
 
 function lottieStateProvider($stateProvider){
@@ -19,11 +27,12 @@ function lottieStateProvider($stateProvider){
     url: 'lottie',
     template: '<span>Lottie wrecks the roost!</span>'
   }
-  
+
   $stateProvider.state('lottie', lottie)
 }
 
 app
   .config(katyStateProvider)
   .config(lottieStateProvider)
+  .config(katyBlogStateProvider)
   .run();
