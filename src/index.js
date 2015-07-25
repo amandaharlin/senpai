7615430313cc358394fbf530375d5ad103893939
Amanda Harlin <amanda.n.harlin@gmail.com>
2015-07-25 11:41:50 -0500

    add angular, angular-ui-router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-global.alert("hello");
+require('angular');
+require('angular-ui-router');
+
+
+var app = angular.module('app', ['ui.router']);
+
+  app.run(
+  function test(){
+    console.log('hello');
+  }
+);
