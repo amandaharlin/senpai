2227bf66af719474cc28c6cc853443383a4730bc
Jessica Campbell <jesslyn.campbell@gmail.com>
2015-08-15 14:32:14 -0500

    Add item-list directive

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('angular-ui-router');
 var app = angular.module('app', ['ui.router']);
 
 
-function katyStateProvider($stateProvider){
+function katyStateProvider($stateProvider) {
   var katy = {
     url: 'katy',
     template: '<span>Katy rules the roost! <div ui-view></div></span>'
@@ -22,7 +22,7 @@ function katyBlogStateProvider($stateProvider) {
   $stateProvider.state('katy.blog', katyBlogState);
 }
 
-function lottieStateProvider($stateProvider){
+function lottieStateProvider($stateProvider) {
   var lottie = {
     url: 'lottie',
     template: '<span>Lottie wrecks the roost!</span>'
@@ -31,8 +31,38 @@ function lottieStateProvider($stateProvider){
   $stateProvider.state('lottie', lottie)
 }
 
+
 app
   .config(katyStateProvider)
   .config(lottieStateProvider)
-  .config(katyBlogStateProvider)
-  .run();
+  .config(katyBlogStateProvider);
+
+
+function itemListDirective() {
+
+  function itemListController($scope) {
+    $scope.id = 1;
+    $scope.value = {
+      somestring: "binding wins!"
+    };
+
+  }
+
+  var scope = {
+    id: "@",
+    value: "=?"
+  };
+
+  return {
+    restrict: 'E',
+    scope: scope,
+    template: '<div>testtesttest{{value.somestring}}</div>',
+    controller: itemListController
+  };
+}
+
+app.directive('itemList', itemListDirective);
+
+//app.controller('itemListController', itemListController($scope));
+
+app.run();
